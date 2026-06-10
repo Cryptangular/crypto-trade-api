d78c5e9f5d0207d50e1f36172277a15187e27950
aliakseidrob <aliakseidrob@gmail.com>
2026-06-10 12:55:27 +0300

    test: update tests

--- src/auth/controllers/auth.controller.spec.ts
+++ src/auth/controllers/auth.controller.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { vi } from 'vitest';
+import { AuthService } from '../services/auth.service';
 import { AuthController } from './auth.controller';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
+  const mockAuthService = {
+    register: vi.fn(),
+    login: vi.fn(),
+    logout: vi.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
