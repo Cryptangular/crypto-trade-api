1b2c0375ba9a62f78b3ee9ff9ff02b67581fcd09
aliakseidrob <aliakseidrob@gmail.com>
2026-06-17 02:01:24 +0300

    refactor: update errors

--- src/auth/constants/auth-errors.ts
+++ src/auth/constants/auth-errors.ts
@@ -1,10 +1,20 @@
+import { AppError } from 'src/shared/types/error.types';
+
 export const AuthErrors = {
   EMAIL_ALREADY_EXISTS: {
-    errorCode: 'AUTH_EMAIL_ALREADY_EXISTS',
+    code: 'AUTH_EMAIL_ALREADY_EXISTS',
     message: 'User with this email already exists',
   },
   INVALID_CREDENTIALS: {
-    errorCode: 'AUTH_INVALID_CREDENTIALS',
+    code: 'AUTH_INVALID_CREDENTIALS',
     message: 'Incorrect email or password',
   },
-} as const;
+  ACCESS_TOKEN_EXPIRED: {
+    code: 'AUTH_ACCESS_TOKEN_EXPIRED',
+    message: 'Access token has expired',
+  },
+  ACCESS_TOKEN_MISSING: {
+    code: 'AUTH_ACCESS_TOKEN_MISSING',
+    message: 'Access token is missing',
+  },
+} as const satisfies Record<string, AppError>;
--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,6 +1,33 @@
-import { Injectable } from '@nestjs/common';
-
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { TokenExpiredError } from 'jsonwebtoken';
+import { AuthErrors } from '../constants/auth-errors';
+import { ActiveUser } from '../types/active-user';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  override handleRequest<TUser = ActiveUser>(
+    err: unknown,
+    user: TUser | false,
+    info: unknown,
+    context: ExecutionContext,
+  ): TUser {
+    console.log(err);
+    console.log(info);
+    console.log(user);
+
+    if (info instanceof TokenExpiredError) {
+      throw new UnauthorizedException(AuthErrors.ACCESS_TOKEN_EXPIRED);
+    }
+
+    if (info instanceof Error && info.message === 'No auth token') {
+      throw new UnauthorizedException(AuthErrors.ACCESS_TOKEN_MISSING);
+    }
+
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
