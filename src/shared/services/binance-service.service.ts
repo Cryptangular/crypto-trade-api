2ada60b2af7a6a1665794fdd5b297acc2c8576b9
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-06-21 15:21:26 +0300

    feat: add simple binance service
    Signed-off-by: AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>

---
+++ src/shared/services/binance-service.service.ts
@@ -0,0 +1,52 @@
+import * as crypto from 'node:crypto';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { SETTINGS_CODES } from 'src/settings/constants/constants';
+import { BINANCE_CONFIG } from '../constants/binance.constants';
+
+@Injectable()
+export class BinanceService {
+  private readonly logger = new Logger(BinanceService.name);
+  private readonly baseUrl = BINANCE_CONFIG.BASE_URL;
+  private readonly httpClient: AxiosInstance;
+
+  constructor() {
+    this.httpClient = axios.create({
+      baseURL: this.baseUrl,
+      timeout: BINANCE_CONFIG.TIMEOUT,
+    });
+  }
+
+  private generateSignature(queryString: string, secretKey: string) {
+    return crypto.createHmac('sha256', secretKey).update(queryString).digest('hex');
+  }
+
+  async testConnection(apiKey: string, secretKey: string): Promise<boolean> {
+    try {
+      const timeRes = await this.httpClient.get(BINANCE_CONFIG.ENDPOINTS.TIME);
+      const serverTime = timeRes.data.serverTime;
+
+      const timestamp = serverTime.toString();
+      const queryString = `timestamp=${timestamp}`;
+
+      const signature = this.generateSignature(queryString, secretKey);
+
+      await this.httpClient.get(BINANCE_CONFIG.ENDPOINTS.ACCOUNT, {
+        params: { timestamp, signature },
+        headers: { [BINANCE_CONFIG.HEADERS.API_KEY]: apiKey },
+      });
+
+      return true;
+    } catch (error) {
+      this.logger.error('Binance connection failed', error instanceof Error ? error.message : String(error));
+
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 401) {
+          throw new BadRequestException(SETTINGS_CODES.INVALID_KEYS);
+        }
+        throw new InternalServerErrorException(SETTINGS_CODES.UNKNOWN_ERROR);
+      }
+      throw new InternalServerErrorException(SETTINGS_CODES.UNKNOWN_ERROR);
+    }
+  }
+}
