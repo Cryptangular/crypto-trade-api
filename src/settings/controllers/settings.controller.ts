65e6eecfdf176a0ba278ce4f67e036da00b6df73
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-06-21 15:22:39 +0300

    feat: add settings controller
    Signed-off-by: AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>

---
+++ src/settings/controllers/settings.controller.ts
@@ -0,0 +1,69 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseFilters,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ActiveUser } from 'src/auth/types/active-user';
+import { SETTINGS_CODES } from '../constants/constants';
+import { SettingsRequestDto, SettingsResponseDTO } from '../dto/settings.dto';
+import { SettingsFilter } from '../exception-filters/settings.filter';
+import { SettingsValidationPipe } from '../pipes/settings-validation.pipe';
+import { SettingsService } from '../services/settings.service';
+import { SettingsResponse } from '../types/types';
+
+@Controller('settings')
+@UseGuards(JwtAuthGuard)
+@UseFilters(SettingsFilter)
+export class SettingsController {
+  constructor(private readonly settingsService: SettingsService) {}
+
+  @Get()
+  async getSettings(@GetUser() user: ActiveUser): Promise<SettingsResponse<SettingsResponseDTO>> {
+    const settings = await this.settingsService.getSettings(user.id);
+
+    if (!settings) {
+      return { data: { apiKey: null, hasSecret: false }, code: SETTINGS_CODES.SETTINGS_GOT };
+    }
+
+    return {
+      data: {
+        apiKey: settings.apiKey,
+        hasSecret: true,
+      },
+      code: SETTINGS_CODES.SETTINGS_GOT,
+    };
+  }
+
+  @Post()
+  @UsePipes(new SettingsValidationPipe())
+  @HttpCode(HttpStatus.OK)
+  async setSettings(
+    @GetUser() user: ActiveUser,
+    @Body() dto: SettingsRequestDto,
+  ): Promise<SettingsResponse<SettingsResponseDTO>> {
+    const { apiKey, secretKey } = await this.settingsService.setSettings(user.id, dto);
+
+    return {
+      data: {
+        apiKey,
+        hasSecret: !!secretKey,
+      },
+      code: SETTINGS_CODES.SETTINGS_SETTED,
+    };
+  }
+
+  @Delete()
+  async removeSettings(@GetUser() user: ActiveUser): Promise<SettingsResponse<null>> {
+    await this.settingsService.removeSettings(user.id);
+    return { data: null, code: SETTINGS_CODES.SETTINGS_REMOVED };
+  }
+}
