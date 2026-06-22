52409852f5f7cc4d701289ac2ba94554b74da0f2
aliakseidrob <aliakseidrob@gmail.com>
2026-06-22 19:45:06 +0300

    test: fix service test

--- src/markets/services/markets.service.spec.ts
+++ src/markets/services/markets.service.spec.ts
@@ -1,15 +1,27 @@
+import { HttpService } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
-import { MarketsServiceService } from './markets.service';
+import { of } from 'rxjs';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { MarketsService } from './markets.service';
 
-describe('MarketsServiceService', () => {
-  let service: MarketsServiceService;
+describe('MarketsService', () => {
+  let service: MarketsService;
+  const mockHttpService = {
+    get: vi.fn().mockReturnValue(of({ data: { symbols: [] } })),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MarketsServiceService],
+      providers: [
+        MarketsService,
+        {
+          provide: HttpService,
+          useValue: mockHttpService,
+        },
+      ],
     }).compile();
 
-    service = module.get<MarketsServiceService>(MarketsServiceService);
+    service = module.get<MarketsService>(MarketsService);
   });
 
   it('should be defined', () => {
