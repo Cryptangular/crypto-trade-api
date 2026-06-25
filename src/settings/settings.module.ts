a21d5f5c3966ea318222880d91f4ca4ff521f099
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-06-26 02:42:07 +0300

    fix: remove redundant providers
    Signed-off-by: AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>

--- src/settings/settings.module.ts
+++ src/settings/settings.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { BinanceService } from 'src/shared/services/binance-service.service';
-import { CipherService } from 'src/shared/services/cipher-service.service';
 import { SharedModule } from 'src/shared/shared.module';
 import { SettingsController } from './controllers/settings.controller';
 import { SettingsService } from './services/settings.service';
@@ -9,6 +7,6 @@ import { SettingsService } from './services/settings.service';
 @Module({
   imports: [PrismaModule, SharedModule],
   controllers: [SettingsController],
-  providers: [SettingsService, CipherService, BinanceService],
+  providers: [SettingsService],
 })
 export class SettingsModule {}
