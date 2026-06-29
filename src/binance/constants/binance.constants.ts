c1fe8492217d1eb72efce706825951222c628964
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-06-29 04:52:25 +0300

    feat: add constants for binance module
    Signed-off-by: AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>

---
+++ src/binance/constants/binance.constants.ts
@@ -0,0 +1,23 @@
+export const BINANCE_HTTP_CLIENT = 'BINANCE_HTTP_CLIENT';
+
+export const BINANCE_CONFIG = {
+  BASE_URL: 'https://testnet.binance.vision',
+  TIMEOUT: 5000,
+  ENDPOINTS: {
+    TIME: '/api/v3/time',
+    ACCOUNT: '/api/v3/account',
+    DEPTH: '/api/v3/depth',
+  },
+  HEADERS: {
+    API_KEY: 'X-MBX-APIKEY',
+  },
+  DEFAULTS: {
+    LIMIT: 100,
+  },
+} as const;
+
+export const BINANCE_CODES = {
+  BINANCE_INVALID_CREDENTIALS: 'BINANCE_INVALID_CREDENTIALS',
+  BINANCE_UNREACHEABLE: 'BINANCE_UNREACHEABLE',
+  UNKNOWN_ERROR: 'UNKNOWN_ERROR',
+} as const;
