da30f0ba87dc52b3eba0597e9c1d21faab63de24
Alexandr Zhdanko <lex.zhdanko@gmail.com>
2026-06-30 18:59:47 +0400

    refactor(trade): replace manual query validation with GetKlinesDto

--- src/trade/controllers/trade.controller.ts
+++ src/trade/controllers/trade.controller.ts
@@ -1,16 +1,16 @@
-import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BinanceService } from 'src/shared/services/binance.service';
 import { CandlestickData } from 'src/shared/types/trade.types';
+import { GetKlinesDto } from '../dto/get-klines.dto';
 
 @Controller('trade')
 export class TradeController {
   constructor(private readonly binanceService: BinanceService) {}
 
   @Get('klines')
-  async getKlines(@Query('symbol') symbol: string, @Query('interval') interval: string): Promise<CandlestickData[]> {
-    if (!symbol || !interval) {
-      throw new BadRequestException('Parameters "symbol" and "interval" are required');
-    }
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async getKlines(@Query() query: GetKlinesDto): Promise<CandlestickData[]> {
+    const { symbol, interval } = query;
 
     const rawData = await this.binanceService.getHistoricalKlines(symbol, interval);
 
