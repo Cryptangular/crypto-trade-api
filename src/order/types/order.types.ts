32d8cef3df730cffe7fb979a2f36497fff63cf73
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-06-30 16:54:37 +0300

    fix: remove redundant types
    Signed-off-by: AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>

--- src/order/types/order.types.ts
+++ src/order/types/order.types.ts
@@ -1,16 +1,5 @@
-export type OrderBookItem = {
-  price: string;
-  quantity: string;
-};
-
 export interface BinanceOrderBookResponse {
   lastUpdateId: number;
   bids: [string, string][];
   asks: [string, string][];
 }
-
-export type BinanceOrderBook = {
-  lastUpdateId: number;
-  bids: OrderBookItem[];
-  asks: OrderBookItem[];
-};
