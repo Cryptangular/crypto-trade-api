fe343801818e2c4401a52e6d2a62618177d1995c
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-07-06 21:43:43 +0300

    feat: add connection status endpoint
    Signed-off-by: AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>

--- src/settings/controllers/settings.controller.ts
+++ src/settings/controllers/settings.controller.ts
@@ -43,6 +43,11 @@ export class SettingsController {
     };
   }
 
+  @Get('connection-status')
+  async getConnectionStatus(@GetUser() user: ActiveUser): Promise<SettingsResponse<void>> {
+    return await this.settingsService.getConnectionStatus(user.id);
+  }
+
   @Post()
   @UsePipes(new SettingsValidationPipe())
   @HttpCode(HttpStatus.OK)
