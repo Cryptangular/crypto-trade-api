ec85eecc9738d2fd705187845fc3345ac4d291dc
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-07-08 00:22:21 +0300

    fix: remove magic number
    Signed-off-by: AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>

--- src/order/services/binance-order.service.ts
+++ src/order/services/binance-order.service.ts
@@ -1,18 +1,15 @@
-import { Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
-import type { AxiosInstance } from 'axios';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { BINANCE_CONFIG } from 'src/binance/config/binance.config';
-import { BINANCE_HTTP_CLIENT } from 'src/binance/constants/binance.constants';
 import { BinanceBaseService } from 'src/binance/services/binance-base.service';
 import { BinanceOrderBookResponse } from 'src/order/types/order.types';
 import { COMMON_ERRORS_CODES } from 'src/shared/constants/common.constants';
 
 @Injectable()
 export class BinanceOrderService extends BinanceBaseService {
-  constructor(@Inject(BINANCE_HTTP_CLIENT) httpClient: AxiosInstance) {
-    super(httpClient);
-  }
-
-  async getOrderBookInitialState(symbol: string, limit = 100): Promise<BinanceOrderBookResponse> {
+  async getOrderBookInitialState(
+    symbol: string,
+    limit = BINANCE_CONFIG.DEFAULTS.LIMIT,
+  ): Promise<BinanceOrderBookResponse> {
     try {
       const { data } = await this.httpClient.get<BinanceOrderBookResponse>(BINANCE_CONFIG.ENDPOINTS.DEPTH, {
         params: {
